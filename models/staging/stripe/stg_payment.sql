620e86a4f6f449afbc470414426491fd5787ab70
sam deleu <sam.deleu@globis-software.com>
2022-10-27 13:31:32 +0000

    .

--- models/staging/stripe/stg_payment.sql
+++
@@ -1,9 +0,0 @@
-with payment as (
-    select ID as payment_id
-     ,orderid as order_id
-     ,paymentmethod as payment_method
-     ,status as payment_status
-     ,amount 
-     from raw.stripe.payment
-)
-select * from payment
